17ec8c1d80d4098deaac749970dca89515f93560
shahzaib420 <muhammad.shahzaib@tabaarakict.org>
2022-12-26 23:58:22 +0500

    updated new changes

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -7,14 +7,30 @@ import { UpdateOrderDto } from './dto/update-order.dto';
 export class OrdersService {
   async getOrders() {
     try {
+      const filteredOrders = [];
       const uri = 'https://pos.globalfoodsoft.com/pos/order/pop';
       // 'Authorization': `Bearer ${xApiKey}`
       const headers = {
         Authorization: 'OdNZMs1bdFbgjjqVZX',
       };
       const test1 = await axios.post(uri, {}, { headers: headers });
-      console.log('test1', test1.data);
-      return test1.data;
+
+      const orders = test1.data.orders;
+
+      console.log('orders', orders);
+
+      for (let i = 0; i < orders.length; i++) {
+        const element = orders[i];
+        const obj = {
+          id: element.id,
+          type: element.type,
+          status: element.status,
+          fulfill_at: element.fulfill_at,
+        };
+        filteredOrders.push(obj);
+      }
+
+      return filteredOrders;
     } catch (error) {
       console.log('error recieved', error);
       throw error;
